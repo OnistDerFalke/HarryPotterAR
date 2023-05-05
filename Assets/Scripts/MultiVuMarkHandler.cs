0454ae4abcb5b22b8e69dfec2f3048ffa8d9d8f2
Onist Der Falke <75864407+OnistDerFalke@users.noreply.github.com>
2023-05-05 13:35:26 +0200

    vumarks, detecting multiple vumarks

---
+++ Assets/Scripts/MultiVuMarkHandler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Vuforia;
+using System.Collections.Generic;
+
+public class MultiVuMarkHandler : DefaultObserverEventHandler
+{
+    private List<string> currentTrackedObjects = new List<string>();
+    [SerializeField] public List<string> availableIds = new List<string>();
+    [SerializeField] public List<GameObject> models = new List<GameObject>();
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        var id = GetComponent<VuMarkBehaviour>().InstanceId.StringValue;
+        if (!currentTrackedObjects.Contains(id))
+        {
+            currentTrackedObjects.Add(id);
+            Debug.Log($"Detected a character: {id}");
+        }
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        var vmb = GetComponent<VuMarkBehaviour>();
+        if (vmb.InstanceId != null)
+        {
+            currentTrackedObjects.Remove(vmb.InstanceId.StringValue);
+            Debug.Log($"Lost tracking on character: {vmb.InstanceId.StringValue}");
+        }
+    }
+
+    void Update()
+    {
+        foreach(var elem in currentTrackedObjects)
+        {
+            foreach (var model in models)
+                model.SetActive(false);
+            models[availableIds.IndexOf(elem)].SetActive(true);
+        }
+    }
+}
