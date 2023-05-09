516f1966b6e96c17f57ab5246140a1fdfceb38d6
Onist Der Falke <75864407+OnistDerFalke@users.noreply.github.com>
2023-05-09 20:07:01 +0200

    left and right swipe mechanism

--- Assets/Scripts/UI/PlayersSwipe.cs
+++ Assets/Scripts/UI/PlayersSwipe.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,73 +8,104 @@ namespace UI
 {
     public class PlayersSwipe : MonoBehaviour
     {
-        [SerializeField] private Text PlayersNumberText;
-        [SerializeField] private float MinDistanceForSwipe = 20f;
-        [SerializeField] private int[] PlayersRange = new int[2];
-    
-        private Vector2 fingerDownPosition, fingerUpPosition;
-        private int numberOfPlayers;
+        public Transform[] optionsTransforms;
+        public GameObject optionsBox;
+        public int currentOptionID = 0;
+        public float optionsOffset = 130f;
+        public float swipeSpeed = 3f;
+        public float snapSpeed = 5f;
+
+        private Vector2 fingerDown;
+        private Vector2 fingerUp;
+        private float swipeThreshold = 50f; 
+        private int currentPosition = 0;
+        private float midDistance;
+
+        private bool snapLock = false;
 
         void Start()
         {
-            numberOfPlayers = PlayersRange[0];
-            UpdateContext();
+            SetValues();
         }
+
         void Update()
         {
-            SwipeCheck();
-        }
-
-        private void UpdateContext()
-        {
-            PlayersNumberText.text = numberOfPlayers.ToString();
-        }
-
-        private void SwipeCheck()
-        {
-            foreach (var touch in Input.touches)
+            if (Input.touchCount != 1) return;
+            
+            var touch = Input.GetTouch(0);
+            switch (touch.phase)
             {
-                switch (touch.phase)
+                case TouchPhase.Began:
+                    snapLock = true;
+                    fingerDown = touch.position;
+                    fingerUp = touch.position;
+                    break;
+                case TouchPhase.Moved:
                 {
-                    case TouchPhase.Began:
-                        fingerDownPosition = touch.position;
-                        fingerUpPosition = touch.position;
-                        break;
-                    case TouchPhase.Moved:
-                        fingerUpPosition = touch.position;
-                        break;
-                    case TouchPhase.Ended:
+                    fingerUp = touch.position;
+                    var distance = fingerDown.x - fingerUp.x;
+                    
+                    if (optionsBox.transform.localPosition.x > 0)
+                        optionsBox.transform.localPosition = new Vector3(
+                            0f, optionsBox.transform.localPosition.y, optionsBox.transform.localPosition.z);
+                    if (optionsBox.transform.localPosition.x < -(optionsTransforms.Length - 1) * optionsOffset)
+                        optionsBox.transform.localPosition = new Vector3(
+                            -(optionsTransforms.Length - 1) * optionsOffset, optionsBox.transform.localPosition.y, optionsBox.transform.localPosition.z);
+                    
+                    Debug.Log($"{optionsBox.transform.localPosition.x} > {-(optionsTransforms.Length - 1) * optionsOffset}");
+                    
+                    if (optionsBox.transform.localPosition.x <= 0 && optionsBox.transform.localPosition.x >=
+                        -(optionsTransforms.Length - 1) * optionsOffset)
                     {
-                        fingerUpPosition = touch.position;
-
-                        if (fingerDownPosition != fingerUpPosition)
-                        {
-                            var swipeDistance = (fingerDownPosition - fingerUpPosition).magnitude;
-                            if (swipeDistance > MinDistanceForSwipe)
-                            {
-                                var swipeDirection = fingerUpPosition - fingerDownPosition;
-                                if (swipeDirection.x < 0)
-                                {
-                                    //Left swipe handle
-                                    numberOfPlayers++;
-                                    if (numberOfPlayers > PlayersRange[1]) 
-                                        numberOfPlayers = PlayersRange[1];
-                                }
-                                else
-                                {
-                                    //Right swipe handle
-                                    numberOfPlayers--;
-                                    if (numberOfPlayers < PlayersRange[0]) 
-                                        numberOfPlayers = PlayersRange[0];
-                                }
-                                
-                                UpdateContext();
-                            }
-                        }
-                        break;
+                        optionsBox.transform.localPosition +=
+                            new Vector3(-distance * swipeSpeed * Time.deltaTime, 0f, 0f);
                     }
+
+                    break;
+                }
+                case TouchPhase.Ended:
+                {
+                    snapLock = false;
+                    fingerUp = touch.position;
+                    var currentPos = optionsBox.transform.localPosition.x;
+                    Debug.Log(currentPos + " " + currentPos / optionsOffset );
+                    int id = (int)Math.Abs(Math.Round(currentPos / optionsOffset));
+                    Debug.Log("id: " +id);
+
+                    StartCoroutine(SnapToPosition(id));
+                    break;
                 }
             }
+        }
+
+        IEnumerator SnapToPosition(int id)
+        {
+            var optionsBoxPos = optionsBox.transform.localPosition;
+            var targetPosition = new Vector3(-id * optionsOffset, optionsBoxPos.y, optionsBoxPos.z);
+            
+            while (transform.position.x + id * optionsOffset != 0)
+            {
+                if (snapLock) break;
+                optionsBox.transform.localPosition = Vector3.MoveTowards(
+                    optionsBox.transform.localPosition, targetPosition, snapSpeed * Time.deltaTime);
+                yield return null;
+            }
+            
+            if(!snapLock)
+                currentOptionID = id;
+        }
+
+        void SetValues()
+        {
+            for (var i = 0; i < optionsTransforms.Length; i++)
+                optionsTransforms[i].localPosition = new Vector3(i * optionsOffset, 
+                    optionsTransforms[i].localPosition.y, optionsTransforms[i].localPosition.z);
+
+            optionsBox.transform.localPosition = Vector3.zero + 
+                                                 new Vector3(
+                                                     -currentOptionID * optionsOffset,
+                                                     optionsBox.transform.localPosition.y,
+                                                     optionsBox.transform.localPosition.z);
         }
     }
 }
