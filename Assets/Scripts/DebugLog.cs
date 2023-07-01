60f6b23b553c45e4efe7fcb89fbd285a9c7f021f
Marcin Myśliborski <75864407+OnistDerFalke@users.noreply.github.com>
2023-07-01 11:25:35 +0200

    android debug log improvement

--- Assets/Scripts/DebugLog.cs
+++ Assets/Scripts/DebugLog.cs
@@ -1,21 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Assets.Scripts;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class DebugLog : MonoBehaviour
 {
+    [SerializeField] private Button hideLogsButton;
+    [SerializeField] private Button upButton;
+    [SerializeField] private Button downButton;
     [SerializeField] private Text debugText;
     [SerializeField] private int logsVisible = 35;
-    
+    private bool logsSeen = true;
+    private int offset = 0;
+
     void Update()
     {
         debugText.text = "";
-        for (var i = GameManager.DebugLogs.Count - logsVisible; i < GameManager.DebugLogs.Count; i++)
+        if (!logsSeen) { }
+        else
         {
-            if (i >= 0)
-                debugText.text += GameManager.DebugLogs[i] + "\n";
+            for (var i = GameManager.DebugLogs.Count - logsVisible - offset; i < GameManager.DebugLogs.Count - offset; i++)
+            {
+                if (i >= 0) debugText.text += GameManager.DebugLogs[i] + "\n";
+            }
         }
+    }
+    
+    private void OnLogsButton()
+    {
+        logsSeen = !logsSeen;
+    }
+
+    private void OnUpButton()
+    {
+        offset++;
+    }
+
+    private void OnDownButton()
+    {
+        if (offset > 0)
+            offset--;
     }
 }
