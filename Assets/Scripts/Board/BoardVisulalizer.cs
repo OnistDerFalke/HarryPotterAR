a3d9606f9d69b447f0a827dac4ecb6159342ca0d
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2023-07-02 15:22:39 +0200

    hiding and showing visuals fixed

--- Assets/Scripts/Board/BoardVisulalizer.cs
+++ Assets/Scripts/Board/BoardVisulalizer.cs
@@ -30,121 +30,122 @@ namespace Assets.Scripts
             corners[3].transform.rotation = referenceRotation;
         }
 
-        //private void TrackHighlights()
-        //{
-        //    foreach((Field f, GameObject g) highlight in fieldHighlights)
-        //    {
-        //        highlight.g.transform.position = converter.ConvertCoordinates(highlight.f.Figure.CenterPosition);
-        //        highlight.g.transform.rotation = referenceRotation;
-        //    }
-        //}
+        private void TrackHighlights()
+        {
+            foreach ((Field f, GameObject g) highlight in fieldHighlights)
+            {
+                highlight.g.transform.position = converter.ConvertCoordinates(highlight.f.Figure.CenterPosition);
+                highlight.g.transform.rotation = referenceRotation;
+            }
+        }
 
-        //private void MakeQuadrangle(MeshFilter meshFilter, Quadrangle q, Vector2 position)
-        //{
-        //    Vector3[] vertices = new Vector3[4];
-        //    vertices[0] = new Vector3(q.leftBottom.x - position.x, 0f, q.leftBottom.y - position.y);
-        //    vertices[1] = new Vector3(q.leftUpper.x - position.x, 0f, q.leftUpper.y - position.y);
-        //    vertices[2] = new Vector3(q.rightUpper.x - position.x, 0f, q.rightUpper.y - position.y);
-        //    vertices[3] = new Vector3(q.rightBottom.x - position.x, 0f, q.rightBottom.y - position.y);
-        //    int[] triangles = new int[6] { 0, 1, 2, 0, 2, 3 };
-        //    Mesh mesh = new Mesh();
-        //    meshFilter.mesh = mesh;
-        //    mesh.vertices = vertices;
-        //    mesh.triangles = triangles;
-        //    mesh.RecalculateBounds();
-        //    mesh.RecalculateNormals();
-        //}
+        private void MakeQuadrangle(MeshFilter meshFilter, Quadrangle q, Vector2 position)
+        {
+            Vector3[] vertices = new Vector3[4];
+            vertices[0] = new Vector3(q.leftBottom.x - position.x, 0f, q.leftBottom.y - position.y);
+            vertices[1] = new Vector3(q.leftUpper.x - position.x, 0f, q.leftUpper.y - position.y);
+            vertices[2] = new Vector3(q.rightUpper.x - position.x, 0f, q.rightUpper.y - position.y);
+            vertices[3] = new Vector3(q.rightBottom.x - position.x, 0f, q.rightBottom.y - position.y);
+            int[] triangles = new int[6] { 0, 1, 2, 0, 2, 3 };
+            Mesh mesh = new Mesh();
+            meshFilter.mesh = mesh;
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+        }
 
-        //private void AdjustParticleEffectSize(Field field, ParticleSystem ps)
-        //{
-        //    //var shape = ps.shape;
-        //    if (field.Figure is Quadrangle)
-        //    {
-        //        Quadrangle q = field.Figure as Quadrangle;
-        //        float shortestSide = Mathf.Min(
-        //        Vector2.Distance(q.leftUpper, q.leftBottom),
-        //        Vector2.Distance(q.leftUpper, q.rightUpper),
-        //        Vector2.Distance(q.rightUpper, q.rightBottom),
-        //        Vector2.Distance(q.leftBottom, q.rightBottom));
-        //        //shape.radius = shortestSide * particleRadiusFactor;
-        //    }
-        //    else if (field.Figure is Circle)
-        //    {
-        //        Circle c = field.Figure as Circle;
-        //        //shape.radius = c.Radius * particleRadiusFactor;
-        //    }
-        //}
+        private void AdjustParticleEffectSize(Field field, ParticleSystem ps)
+        {
+            //var shape = ps.shape;
+            if (field.Figure is Quadrangle)
+            {
+                Quadrangle q = field.Figure as Quadrangle;
+                float shortestSide = Mathf.Min(
+                Vector2.Distance(q.leftUpper, q.leftBottom),
+                Vector2.Distance(q.leftUpper, q.rightUpper),
+                Vector2.Distance(q.rightUpper, q.rightBottom),
+                Vector2.Distance(q.leftBottom, q.rightBottom));
+                //shape.radius = shortestSide * particleRadiusFactor;
+            }
+            else if (field.Figure is Circle)
+            {
+                Circle c = field.Figure as Circle;
+                //shape.radius = c.Radius * particleRadiusFactor;
+            }
+        }
 
-        //private void MakeCircle(MeshFilter meshFilter, int numOfPoints, Circle c, Vector2 position)
-        //{
-        //    float angleStep = 360.0f / (float)numOfPoints;
-        //    List<Vector3> vertexList = new List<Vector3>();
-        //    List<int> triangleList = new List<int>();
-        //    Quaternion quaternion = Quaternion.Euler(0.0f, angleStep, 0.0f);
-        //    vertexList.Add(new Vector3(0.0f, 0.0f, 0.0f));  
-        //    vertexList.Add(new Vector3(0.0f, 0.0f, c.Radius));  
-        //    vertexList.Add(quaternion * vertexList[1]);    
-                                                          
-        //    triangleList.Add(0);
-        //    triangleList.Add(1);
-        //    triangleList.Add(2);
-        //    for (int i = 0; i < numOfPoints - 1; i++)
-        //    {
-        //        triangleList.Add(0);                     
-        //        triangleList.Add(vertexList.Count - 1);
-        //        triangleList.Add(vertexList.Count);
-        //        vertexList.Add(quaternion * vertexList[vertexList.Count - 1]);
-        //    }
-        //    Mesh mesh = new Mesh();
-        //    meshFilter.mesh = mesh;
-        //    mesh.vertices = vertexList.ToArray();
-        //    mesh.triangles = triangleList.ToArray();
-        //    mesh.RecalculateBounds();
-        //    mesh.RecalculateNormals();
-        //}
+        private void MakeCircle(MeshFilter meshFilter, int numOfPoints, Circle c, Vector2 position)
+        {
+            float angleStep = 360.0f / (float)numOfPoints;
+            List<Vector3> vertexList = new List<Vector3>();
+            List<int> triangleList = new List<int>();
+            Quaternion quaternion = Quaternion.Euler(0.0f, angleStep, 0.0f);
+            vertexList.Add(new Vector3(0.0f, 0.0f, 0.0f));
+            vertexList.Add(new Vector3(0.0f, 0.0f, c.Radius));
+            vertexList.Add(quaternion * vertexList[1]);
 
-        //private void AssignFieldMesh(GameObject highlight, Field field)
-        //{
-        //    MeshFilter meshFilter = highlight.GetComponent<MeshFilter>();
-        //    if (field.Figure is Quadrangle)
-        //    {
-        //        Quadrangle q = field.Figure as Quadrangle;
-        //        MakeQuadrangle(meshFilter, q, field.Figure.CenterPosition);
-        //    }
-        //    else if (field.Figure is Circle)
-        //    {
-        //        Circle c = field.Figure as Circle;
-        //        MakeCircle(meshFilter, 20, c, field.Figure.CenterPosition);
-        //    }
-        //    AdjustParticleEffectSize(field, highlight.GetComponent<ParticleSystem>());
-        //}
+            triangleList.Add(0);
+            triangleList.Add(1);
+            triangleList.Add(2);
+            for (int i = 0; i < numOfPoints - 1; i++)
+            {
+                triangleList.Add(0);
+                triangleList.Add(vertexList.Count - 1);
+                triangleList.Add(vertexList.Count);
+                vertexList.Add(quaternion * vertexList[vertexList.Count - 1]);
+            }
+            Mesh mesh = new Mesh();
+            meshFilter.mesh = mesh;
+            mesh.vertices = vertexList.ToArray();
+            mesh.triangles = triangleList.ToArray();
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+        }
+
+        private void AssignFieldMesh(GameObject highlight, Field field)
+        {
+            MeshFilter meshFilter = highlight.GetComponent<MeshFilter>();
+            if (field.Figure is Quadrangle)
+            {
+                Quadrangle q = field.Figure as Quadrangle;
+                MakeQuadrangle(meshFilter, q, field.Figure.CenterPosition);
+            }
+            else if (field.Figure is Circle)
+            {
+                Circle c = field.Figure as Circle;
+                MakeCircle(meshFilter, 20, c, field.Figure.CenterPosition);
+            }
+            AdjustParticleEffectSize(field, highlight.GetComponent<ParticleSystem>());
+        }
 
         public void HighlightField(Field f)
         {
             Debug.Log($"highlighting {f.Index}");
-            //GameObject highlight = Instantiate(highlightPrefab, transform);
-            //AssignFieldMesh(highlight, f);
-            //fieldHighlights.Add((f, highlight));
+            GameObject highlight = Instantiate(highlightPrefab, transform);
+            AssignFieldMesh(highlight, f);
+            fieldHighlights.Add((f, highlight));
         }
 
         public void UnhighlightField(Field f)
         {
-            //(Field, GameObject)? highlight = fieldHighlights.Find((e) => e.Item1 == f);
-            //if (highlight.HasValue)
-            //{
-            //    Destroy(highlight.Value.Item2);
-            //    fieldHighlights.Remove(highlight.Value);
-            //}
+            List<(Field, GameObject)> highlights = fieldHighlights.FindAll((e) => e.Item1 == f);
+            foreach (var h in highlights)
+            {
+                Debug.Log($"unhighlighting {f.Index}");
+                Destroy(h.Item2);
+                fieldHighlights.Remove(h);
+            }
         }
 
         public void HideVisuals()
         {
-            foreach(var highlight in fieldHighlights)
+            Debug.Log("HIDE VISUALS!");
+            foreach (var highlight in fieldHighlights)
             {
-                Destroy(highlight.Item2);
-                fieldHighlights.Remove(highlight);
+                highlight.Item2.SetActive(false);
             }
-            for(int i = 0; i< 4; i++)
+            for (int i = 0; i < 4; i++)
             {
                 corners[i].SetActive(false);
             }
@@ -152,6 +153,11 @@ namespace Assets.Scripts
 
         public void ShowVisuals()
         {
+            Debug.Log("SHOW VISUALS!");
+            foreach (var highlight in fieldHighlights)
+            {
+                highlight.Item2.SetActive(true);
+            }
             for (int i = 0; i < 4; i++)
             {
                 corners[i].SetActive(true);
@@ -160,14 +166,14 @@ namespace Assets.Scripts
 
         private void Awake()
         {
-           
+
         }
 
         private void Update()
         {
             referenceRotation = converter.ReferenceRotation();
             TrackCorners();
-            //TrackHighlights();
+            TrackHighlights();
         }
     }
 }
